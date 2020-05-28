e6a075598776cdcceb8ffa2d5f14b4b2c47b62db
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-28 18:12:49 +0300

    refactored and added new model

--- back/models/index.js
+++ back/models/index.js
@@ -4,23 +4,8 @@ import associate from "./association"
 const breed = orm.import("breed", require("./breed"))
 const fact = orm.import("fact", require("./fact"))
 const emoji = orm.import("emoji", require("./emoji"))
+const image = require("./image").default
 
 associate({breed,fact,emoji})
 
-export { breed as BreedModel, fact as FactModel, emoji as EmojiModel }
-
-// export default db
-
-// const models = {
-//     Breed: Breed.init(orm, DataTypes)
-// }
-// const Fact = orm.sequelize.import("./fact")
-// const CountBreed = orm.sequelize.import("./countBreed")
-// const CountFact = orm.sequelize.import("./CountFact")
-// associate({ Breed, Fact, CountBreed, CountFact })
-// export {
-//   Breed as BreedModel,
-//   Fact as FactModel,
-//   CountBreed as CountBreedModel,
-//   CountFact as CountFactModel,
-// }
+export { breed as BreedModel, fact as FactModel, emoji as EmojiModel, image as ImageModel }
