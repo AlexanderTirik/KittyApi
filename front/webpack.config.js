b426f3ac2dd1e25b1a43a745e49d31064ca0cc4d
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-30 18:37:17 +0300

    webpack config

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,8 +1,9 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const webpack = require("webpack")
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: ["./src/index.js", "@babel/polyfill"],
   output: {
     path: path.join(__dirname, "/dist"),
     filename: "index_bundle.js",
@@ -18,6 +19,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ["url-loader", "file-loader"],
+      },
     ],
   },
   devServer: {
@@ -28,6 +33,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify("PRODUCTION"),
+      API_URL: JSON.stringify("http://127.0.0.1"),
+      API_PORT: JSON.stringify("3001"),
     }),
   ],
   devServer: {
