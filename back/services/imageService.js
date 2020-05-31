33c3109f41fb7fac3884be6e5a917c16869258ec
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-31 15:59:36 +0300

    new method in service

--- back/services/imageService.js
+++ back/services/imageService.js
@@ -2,17 +2,33 @@ class ImageServise {
   constructor(model) {
     this.model = model
     this.getRandom = this.getRandom.bind(this)
+    this.getOne = this.getOne.bind(this)
   }
 
   async getRandom(where) {
     try {
       const image = await this.model.findRandom(where)
-      return {
-        error: false,
-        data: image,
-        statusCode: 200,
-      }
+      return image
     } catch (error) {
+      return {
+        error: true,
+        message: error.message,
+        statusCode: 500,
+      }
+    }
+  }
+  async getOne(where) {
+    try {
+      const image = await this.model.getOne(where)
+      return image
+    } catch (error) {
+      if (error.message == "file not found") {
+        return {
+          error: true,
+          message: error.message,
+          statusCode: 404,
+        }
+      }
       return {
         error: true,
         message: error.message,
