432f893e206e6d37591adcd3fb04d9146fcd6314
AlexanderTirik <sasha.tirik@gmail.com>
2020-05-31 23:44:09 +0300

    rewrited component

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,16 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
+import MainPage from "./MainPage/MainPage"
+import "./App.css"
 
-import './App.css';
 class App extends Component {
-    
-    
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <MainPage/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
